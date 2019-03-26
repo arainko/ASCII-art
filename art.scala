09137316dc5c78282f0f5b2ad78ef63132411d18
Aleksander Rainko <aleksander.rainko99@gmail.com>
2019-03-26 23:18:57 +0100

    code cleanup

--- art.scala
+++ art.scala
@@ -1,50 +1,62 @@
 import java.io.File
 import javax.imageio.ImageIO
 import java.awt.image.BufferedImage
+import scala.annotation.tailrec
 
 object ASCIIart extends App{
-    val img = ImageIO.read(new File("image.jpg"))
+
+    print("Image filepath: ")
+    val img = ImageIO.read(new File(io.StdIn.readLine))
     
-    def avgColorTable(img:BufferedImage) : Seq[Seq[Int]] = {
-        def tableHelper(img:BufferedImage, col:Int, row:Int, tempOut: Seq[Int], output:Seq[Seq[Int]]) : Seq[Seq[Int]] = {
+    def getBrightnessMatrix(img:BufferedImage): Seq[Seq[Int]] = {
+        @tailrec
+        def matrixHelper(img:BufferedImage, col:Int, row:Int, tempOut: Seq[Int], output:Seq[Seq[Int]]) : Seq[Seq[Int]] = {
             val color = img.getRGB(col,row)
-                val red = (color << 8) >>> 24
-                val green = (color << 16) >>> 24
-                val blue = (color << 24) >>> 24
-                    val avg = (red+green+blue)/3
+            val red = (color << 8) >>> 24
+            val green = (color << 16) >>> 24
+            val blue = (color << 24) >>> 24
+            val avg = (red+green+blue)/3
 
             if (col == img.getWidth-1 && row == img.getHeight-1) output :+ (tempOut :+ avg)
-            else if (col == img.getWidth-1) tableHelper(img, 0, row+1, Seq.empty[Int], output :+ tempOut)
-            else tableHelper(img, col+1, row, tempOut :+ avg, output)
+            else if (col == img.getWidth-1) matrixHelper(img, 0, row+1, Seq.empty[Int], output :+ tempOut)
+            else matrixHelper(img, col+1, row, tempOut :+ avg, output)
         }
-        tableHelper(img, 0, 0, Seq.empty[Int], Seq.empty[Seq[Int]])
+        matrixHelper(img, 0, 0, Seq.empty[Int], Seq.empty[Seq[Int]])
     }
 
-    def tableConvert(seq:Seq[Seq[Int]]) : Seq[Seq[Char]] = {
+    def matrixConvert(seq:Seq[Seq[Int]]): Seq[Seq[Char]] = {
+        @tailrec
         def convertHelper(seq: Seq[Seq[Int]], insideSeq: Seq[Int], index:Int, tempOut:Seq[Char], output: Seq[Seq[Char]]) : Seq[Seq[Char]] = {
             val encodeSeq= "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$".toSeq
-
             if (seq.tail.isEmpty) output
-            else if (insideSeq.isEmpty) convertHelper(seq.tail, seq.tail.head, 0, Seq.empty[Char], output :+ (tempOut :+ '\n'))
-            else if (insideSeq.head >= (index*4)-4 && insideSeq.head < index*4) convertHelper(seq, insideSeq.tail, 0, tempOut :+ encodeSeq(index) :+ encodeSeq(index) :+  encodeSeq(index), output)
+            else if (insideSeq.isEmpty) convertHelper(seq.tail, seq.tail.head, 1, Seq.empty[Char], output :+ (tempOut :+ '\n'))
+            else if (insideSeq.head >= (index*4)-4 && insideSeq.head < index*4) convertHelper(seq, insideSeq.tail, 1, tempOut :+ encodeSeq(index) :+ encodeSeq(index) :+  encodeSeq(index), output)
             else convertHelper(seq, insideSeq, index+1, tempOut, output)
-
         }
-        convertHelper(seq, seq.head, 0, Seq.empty[Char], Seq.empty[Seq[Char]])
+        convertHelper(seq, seq.head, 1, Seq.empty[Char], Seq.empty[Seq[Char]])
     }
 
-    val a = tableConvert(avgColorTable(img))
-    var i = 0
-    var j = 0
-    while (i < a.size){
-        while(j < a(0).size){
-            print(a(i)(j))
-            j += 1
+    def printMatrix[A](seq: Seq[Seq[A]]): Unit = {
+        @tailrec
+        def printHelper(seq: Seq[Seq[A]], tempSeq: Seq[A]):  Unit = {
+            seq.tail.isEmpty match {
+                case true => {
+                    print(seq.head.mkString(""))
+                    return ()
+                }
+                case false => {
+                    print(tempSeq.mkString(""))
+                    printHelper(seq.tail, seq.tail.head)
+                }
+            }
         }
-        j = 0
-        i += 1
+        printHelper(seq, seq.head)
     }
+
+    val brightnessMatrix = getBrightnessMatrix(img)
+    val brightnessToCharMatrix = matrixConvert(brightnessMatrix)
+    printMatrix(brightnessToCharMatrix)
     
-    }
+}
 
  
